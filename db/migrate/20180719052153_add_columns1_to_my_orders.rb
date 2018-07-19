1fc6b6fcceefb333741a632ff97d80ddd8b023d6
yongzheng <yongzheng9405@gmail.com>
2018-07-19 05:36:16 +0000

    fix bug in modify myorder table

--- db/migrate/20180719052153_add_columns1_to_my_orders.rb
+++ db/migrate/20180719052153_add_columns1_to_my_orders.rb
@@ -1,10 +1,10 @@
 class AddColumns1ToMyOrders < ActiveRecord::Migration[5.2]
   def change
-    add_column :my_orders, :Taxes, :float
-    add_column :my_orders, :Total, :float
-    add_column :my_orders, :Discount_Code, :string
-    add_column :my_orders, :Discount_Amount, :float
-    add_column :my_orders, :Shipping_Method, :string
+    # add_column :my_orders, :Taxes, :float
+    # add_column :my_orders, :Total, :float
+    # add_column :my_orders, :Discount_Code, :string
+    # add_column :my_orders, :Discount_Amount, :float
+    # add_column :my_orders, :Shipping_Method, :string
     # add_column :my_orders, :Created_at, :datetime
     add_column :my_orders, :Lineitem_quantity, :integer
     add_column :my_orders, :Lineitem_name, :string
