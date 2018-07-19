b4dd724dfc444f51e0b3c1c7d1e1321180bb6399
yongzheng <yongzheng9405@gmail.com>
2018-07-19 00:06:07 +0000

    add write orders to shopify_app

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "2ce11a2ac2cfb3975507e23a64909b7d"
   config.secret = "d012148737bd3aa5aa1fdc4e93dbeecc"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_orders, read_products, write_orders"
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
