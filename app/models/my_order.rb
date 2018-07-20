48a184e4a7f649dae8ea5dc5768094b5fc60e4ad
yongzheng <yongzheng9405@gmail.com>
2018-07-20 20:41:34 +0000

    create shopify order

--- app/models/my_order.rb
+++ app/models/my_order.rb
@@ -9,17 +9,24 @@ class MyOrder < ApplicationRecord
   end
 
   def self.import(file)
+    shop = Shop.first
+    session = ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token, '')
+    ShopifyAPI::Base.activate_session(session)
     CSV.foreach(file.path, headers: true, encoding: 'iso-8859-1:utf-8') do |row|
       # product_hash = row.to_hash
       # product = find_or_create_by!(name: product_hash['name'], category: product_hash['category'])
       # product.update_attributes(product_hash)
-      shop = Shop.first
-      puts shop.shopify_domain
-      puts row.to_hash["Email"]
-      session = ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token, '')
-      ShopifyAPI::Base.activate_session(session)
+      # shop = Shop.first
+      # puts shop.shopify_domain
+      # puts row.to_hash["Email"]
+      # session = ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token, '')
+      # ShopifyAPI::Base.activate_session(session)
       MyOrder .create! row.to_hash
-      ShopifyAPI::Order .create!(email:row.to_hash["Email"], line_items: ShopifyAPI::LineItem.create(title:"dummy",quantity:1))
+      #ShopifyAPI::Order .create!(email:row.to_hash["Email"], line_items: [ShopifyAPI::LineItem.new(title:"dummy",quantity:1)])
+      ShopifyAPI::Order.create(email:row.to_hash["Email"],fulfillment_status:"fulfilled",send_receipt:true,send_fulfillment_receipt:true,
+      total_price:"2333.00",subtotal:"2331.00",total_tax:"0.00","currency":"USD",financial_status:"pending",
+      line_items:[ShopifyAPI::LineItem.new(title:"dummy",quantity:1,price:"2333.00",sku:"0")],confirmed:true,
+      total_discounts:"0.00",total_line_items_price:"2333.00",total_price_usd:"2333.00") 
     end
   end
 end
