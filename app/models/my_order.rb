3df5cce15a1e7016767194fa68a81d91a57856e8
yongzheng <yongzheng9405@gmail.com>
2018-07-23 18:09:40 +0000

    fix bug 2 in algo

--- app/models/my_order.rb
+++ app/models/my_order.rb
@@ -43,7 +43,7 @@ class MyOrder < ApplicationRecord
       else
         ShopifyAPI::Order.create(email:pre_email, order_number:pre_order_number, line_items:pre_line_item, shipping_address: pre_address)
         pre_line_item.clear
-        pre_line_item.push[ShopifyAPI::LineItem.new(name:row.to_hash["Lineitem_name"], price:row.to_hash["Lineitem_price"], properties:[name:"size", value:row.to_hash["Notes"]])]
+        pre_line_item.push(ShopifyAPI::LineItem.new(name:row.to_hash["Lineitem_name"], price:row.to_hash["Lineitem_price"], properties:[name:"size", value:row.to_hash["Notes"]]))
         
         pre_address = ShopifyAPI::ShippingAddress.new(address1:row.to_hash["Shipping_Address1"], address2:row.to_hash["Shipping_Address2"],
         province:row.to_hash["Shipping_Province"], country_code:row.to_hash["Shipping_Country"], zip:row.to_hash["Shipping_Zip"])
