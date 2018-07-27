c757c9ffb7b40e8eacc7fde8d5d0f20feec178ba
yongzheng <yongzheng9405@gmail.com>
2018-07-27 17:39:31 +0000

    skip return and modify create time

--- app/models/my_order.rb
+++ app/models/my_order.rb
@@ -15,8 +15,10 @@ class MyOrder < ApplicationRecord
     
     pre_order_number = nil
     CSV.foreach(file.path, headers: true, encoding: 'iso-8859-1:utf-8') do |row|
-      pre_order_number = row.to_hash["OrderId"]
-      break
+      if(row.to_hash["Return"] != "yes")
+        pre_order_number = row.to_hash["OrderId"]
+        break
+      end
     end
     pre_line_item = []
     pre_email = nil
@@ -24,13 +26,16 @@ class MyOrder < ApplicationRecord
     
     pre_total_price = 0
     string_pre_total_price = nil
-      ShopifyAPI::Order.create!(email:"goooo@example.com",fulfillment_status:"fulfilled",send_receipt:true,send_fulfillment_receipt:true,
-      total_price:"2333.00",subtotal:"2331.00",total_tax:"0.00","currency":"USD",financial_status:"pending",
-      line_items:[ShopifyAPI::LineItem.new(title:"dummy",quantity:1,price:"2333.00",sku:"0"),ShopifyAPI::LineItem.new(title:"zzzzzz",quantity:1,price:"2333.00",sku:"0"),],confirmed:true,
-      total_discounts:"0.00",total_line_items_price:"2333.00",total_price_usd:"2333.00", 
-      address: [ShopifyAPI::ShippingAddress.new(first_name:"go", last_name:"went",address1:"123", address2:"345",
-        province:"345", country_code:"4444444", zip:"555555555", phone:"333-444-5555")]) 
+      # ShopifyAPI::Order.create!(email:"goooo@example.com",fulfillment_status:"fulfilled",send_receipt:true,send_fulfillment_receipt:true,
+      # total_price:"2333.00",subtotal:"2331.00",total_tax:"0.00","currency":"USD",financial_status:"pending",
+      # line_items:[ShopifyAPI::LineItem.new(title:"dummy",quantity:1,price:"2333.00",sku:"0"),ShopifyAPI::LineItem.new(title:"zzzzzz",quantity:1,price:"2333.00",sku:"0"),],confirmed:true,
+      # total_discounts:"0.00",total_line_items_price:"2333.00",total_price_usd:"2333.00", 
+      # address: [ShopifyAPI::ShippingAddress.new(first_name:"go", last_name:"went",address1:"123", address2:"345",
+      #   province:"345", country_code:"4444444", zip:"555555555", phone:"333-444-5555")])
     CSV.foreach(file.path, headers: true, encoding: 'iso-8859-1:utf-8') do |row|
+      if(row.to_hash["Return"] == "yes")
+        next
+      end
       # product_hash = row.to_hash
       # product = find_or_create_by!(name: product_hash['name'], category: product_hash['category'])
       # product.update_attributes(product_hash)
@@ -42,10 +47,10 @@ class MyOrder < ApplicationRecord
       #MyOrder .create! row.to_hash
       #puts "import success"
       #ShopifyAPI::Order .create!(email:row.to_hash["Email"], line_items: [ShopifyAPI::LineItem.new(title:"dummy",quantity:1)])
-      # ShopifyAPI::Order.create(email:row.to_hash["Email"],fulfillment_status:"fulfilled",send_receipt:true,send_fulfillment_receipt:true,
-      # total_price:"2333.00",subtotal:"2331.00",total_tax:"0.00","currency":"USD",financial_status:"pending",
-      # line_items:[ShopifyAPI::LineItem.new(title:"dummy",quantity:1,price:"2333.00",sku:"0"),ShopifyAPI::LineItem.new(title:"zzzzzz",quantity:1,price:"2333.00",sku:"0"),],confirmed:true,
-      # total_discounts:"0.00",total_line_items_price:"2333.00",total_price_usd:"2333.00") 
+      ShopifyAPI::Order.create(email:row.to_hash["Email"],fulfillment_status:"fulfilled",send_receipt:true,send_fulfillment_receipt:true,
+      total_price:"2333.00",subtotal:"2331.00",total_tax:"0.00",currency:"USD",financial_status:"pending",
+      line_items:[ShopifyAPI::LineItem.new(title:"dummy",quantity:1,price:"2333.00",sku:"0"),ShopifyAPI::LineItem.new(title:"zzzzzz",quantity:1,price:"2333.00",sku:"0"),],confirmed:true,
+      total_discounts:"0.00",total_line_items_price:"2333.00",total_price_usd:"2333.00") 
       
       
       cur_order_number = row.to_hash["OrderId"]
@@ -72,9 +77,9 @@ class MyOrder < ApplicationRecord
         
         
         ShopifyAPI::Order.create!(email:pre_email,fulfillment_status:"fulfilled",send_receipt:true,send_fulfillment_receipt:true,
-        total_price:string_pre_total_price,subtotal:string_pre_total_price,total_tax:"0.00","currency":"USD",financial_status:"pending",
+        total_price:string_pre_total_price,subtotal:string_pre_total_price,total_tax:"0.00",currency:"USD",financial_status:"pending",
         line_items:pre_line_item,confirmed:true,
-        total_discounts:"0.00",total_line_items_price:"2333.00",total_price_usd:"2333.00", shipping_address: pre_address) 
+        total_discounts:"0.00",total_line_items_price:"2333.00",total_price_usd:"2333.00", shipping_address: pre_address, created_at:row.to_hash["OrderDate"]) 
         
         pre_total_price = row.to_hash["Lineitem_price"].to_f
         
@@ -90,7 +95,7 @@ class MyOrder < ApplicationRecord
     end
     string_pre_total_price = pre_total_price.to_s
     ShopifyAPI::Order.create!(email:pre_email,fulfillment_status:"fulfilled",send_receipt:true,send_fulfillment_receipt:true,
-        total_price:string_pre_total_price,subtotal:string_pre_total_price,total_tax:"0.00","currency":"USD",financial_status:"pending",
+        total_price:string_pre_total_price,subtotal:string_pre_total_price,total_tax:"0.00",currency:"USD",financial_status:"pending",
         line_items:pre_line_item,confirmed:true,
         total_discounts:"0.00",total_line_items_price:"2333.00",total_price_usd:"2333.00",shipping_address: pre_address)
   end
